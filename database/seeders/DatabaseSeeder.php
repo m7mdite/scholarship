81ec43ff122d35522db35563afd8e2cf06b6cbe3
m7mdite <abomos3ab737@gmail.com>
2026-05-16 02:19:20 -0700

    اصلاح الخطأ

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,10 +17,7 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        
         $this->call(CountrySeeder::class);
         $this->call(CitySeeder::class);
         $this->call(CategorySeeder::class);
